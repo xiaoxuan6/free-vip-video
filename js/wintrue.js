844149de0f3560b688fa1815d808bc0e8a8e636a
xiaoxuan6 <1527736751@qq.com>
2023-04-27 18:50:26 +0800

    fix: Update README.md

--- js/wintrue.js
+++ js/wintrue.js
@@ -6,26 +6,23 @@
  * wh震动幅度px
  * fx动画速度s
  */
-function flash(obj,time,wh,fx)
-{ 
-	$(function(){
-	var $panel = $(obj);
-	var offset = $panel.offset()-$panel.width();
-	var x= offset.left;
-	var y= offset.top;
-	for(var i=1; i<=time; i++){
-		if(i%2==0)
-		{
-			$panel.animate({left:'+'+wh+'px'},fx);
-		}else
-		{
-			$panel.animate({left:'-'+wh+'px'},fx);
-		}
-			
-	}
-	$panel.animate({left:0},fx);
-	$panel.offset({ top: y, left: x });
-		
-	})
+function flash(obj, time, wh, fx) {
+    $(function () {
+        var $panel = $(obj);
+        var offset = $panel.offset() - $panel.width();
+        var x = offset.left;
+        var y = offset.top;
+        for (var i = 1; i <= time; i++) {
+            if (i % 2 == 0) {
+                $panel.animate({left: '+' + wh + 'px'}, fx);
+            } else {
+                $panel.animate({left: '-' + wh + 'px'}, fx);
+            }
+
+        }
+        $panel.animate({left: 0}, fx);
+        $panel.offset({top: y, left: x});
+
+    })
 }
 
